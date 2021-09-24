0cfdff5134e23a5e47be68937f4e0c1f175b1f15
richieyang8813@gmail.com <richieyang8813@gmail.com>
2021-09-24 20:21:16 +0800

    add some progress on index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+const background = document.querySelector('#alpaca-background')
+const ear = document.querySelector('#alpaca-ear')
+const hair = document.querySelector('#alpaca-hair')
+const eyes = document.querySelector('#alpaca-eyes')
+const nose = document.querySelector('#alpaca-nose')
+const mouth = document.querySelector('#alpaca-mouth')
+const leg = document.querySelector('#alpaca-leg')
+const accessories = document.querySelector('#alpaca-accessories')
+
+const alpacaDict = {
+  accessories: ['earings', 'flower', 'glasses', 'headphone'],
+  background: []
+}
